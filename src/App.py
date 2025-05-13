9fdedd1b1ed66ce90e4da82a28a464948fe1f2de
Mariano Hernan Gazzola <mhgazz@gmail.com>
2025-05-13 23:02:42 +0000

    Requirements

--- src/App.py
+++ src/App.py
@@ -40,4 +40,4 @@ def efemerides_now():
 if __name__ == '__main__':
     # run() method of Flask class runs the application 
     # on the local development server.
-    app.run()
+    app.run(host='0.0.0.0', port=5000)
